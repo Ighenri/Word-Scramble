af0673dd025ff6502e543c8eab42f9aa90c78431
henry <igwehenry740@gmail.com>
2022-08-31 15:04:01 +0100

    remove span word in html, made the hint span functional on js

--- script.js
+++ script.js
@@ -117,20 +117,19 @@ const words = [
 
 
 const wordText = document.querySelector(".word");
+hintText = document.querySelector(".hint span")
 
 const initGame = () => {
     let randomObj = words[Math.floor(Math.random() * words.length)]; /*getting random object from words*/
     let wordArray = randomObj.word.split(""); /*splitting each letter of random word*/
     for (let i = wordArray.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = wordArray[i];
-        wordArray[i] = wordArray[j];
-        wordArray[j] = temp;  //getting random number
+        let j = Math.floor(Math.random() * (i + 1));   //getting random number
         // shuffling and swiping wordArray letters randomly
        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]]; 
     }
 
     wordText.innerText = wordArray.join(""); //passing shuffled word as word text
-    console.log(wordArray, randomObj.word);
+    hintText.innerText = randomObj.hint; // passing random object hint as hint text
+    console.log(randomObj);
 } 
 initGame();
